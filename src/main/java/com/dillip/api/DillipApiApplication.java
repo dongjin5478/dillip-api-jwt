e30524e7a9ec643f6156dd1e067d3c8fcbe0439a
dongjin5478 <dongjinmaster9@gmail.com>
2022-09-02 13:19:42 +0530

    extended to SpringBootServletInitializer

--- src/main/java/com/dillip/api/DillipApiApplication.java
+++ src/main/java/com/dillip/api/DillipApiApplication.java
@@ -2,6 +2,8 @@ package com.dillip.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,7 +11,7 @@ import com.google.gson.Gson;
 
 
 @SpringBootApplication
-public class DillipApiApplication {
+public class DillipApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DillipApiApplication.class, args);
@@ -25,6 +27,11 @@ public class DillipApiApplication {
 	public Gson getGson()
 	{
 		return new Gson();
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 
 }
